1f34881c1f5d3ae4e0e32578cc8ddf5fb41dc232
Nur <olimovnurullo2001@gmail.com>
2022-03-19 20:22:09 +0500

    Second init

--- app/src/main/java/com/example/currencyrate/MainActivity.kt
+++ app/src/main/java/com/example/currencyrate/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.currencyrate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currencyrate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.qwe.text = " 1 "
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
